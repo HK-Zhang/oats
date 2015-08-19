38e9812becb39be05886dfd61b9eff5b7ccd0ca6
HK Zhang <yxzhk@hotmail.com>
2015-08-19 21:44:12 +0800

    update web request logic

--- WebSpider/Program.cs
+++ WebSpider/Program.cs
@@ -11,7 +11,7 @@ namespace WebSpider
         static void Main(string[] args)
         {
 
-
+            System.Net.ServicePointManager.DefaultConnectionLimit = 50;
             URLManager.PrepareURLPool();
             //URLPool.Push("http://nufm3.dfcfw.com/EM_Finance2014NumericApplication/JS.aspx?type=CT&cmd=0022012&sty=FDT&st=z&sr=&p=&ps=&lvl=&cb=&js=var%20jsquote=(x);&token=beb0a0047196124721f56b0f0ff5a27c&rt=0.6847063523412638");
             ReaderJobManger readerJob = new ReaderJobManger();
